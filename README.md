ecd6dbd15fdbd4f8aa1f10e73879383ea34ecc1b
Sergio Sampedro <4csergio10@gmail.com>
2024-03-20 16:16:31 +0100

    First version

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # FastAPI-medium-example
+
+In this repo we found a simple example of how to start using FastAPI to made APIs.
+
+## Set new env conda
+```sh
+conda remove --name fastapi --all
+```
+
+```sh
+conda create -n fastapi python=3.9
+conda activate fastapi
+pip install -r requirements.txt
+
+## Run ML API locally
+
+```sh
+($) python -m src.app
+```
---
+++ src/routers/router_1.py
@@ -0,0 +1,69 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException
+
+import src.auth as auth
+from src.db import DB, get_db
+from src.models import Compute, Compute_all, Mode, Results
+
+router = APIRouter(prefix="/calculator")
+
+
+@router.post("/compute/", response_model=Compute)
+def read_heroes(
+    number: int,
+    mode: Mode = "SUM",
+    db: DB = Depends(get_db),
+    authorized: bool = Depends(auth.validate),
+) -> Compute:
+    db_num = db.get_number()
+
+    if mode == "SUM":
+        result = Results(addition=(number + db_num))
+    elif mode == "SUB":
+        result = Results(subtraction=(number - db_num))
+    elif mode == "PROD":
+        result = Results(multiplication=(number * db_num))
+    elif mode == "ALL":
+        result = Results(
+            addition=(number + db_num),
+            subtraction=(number - db_num),
+            multiplication=(number * db_num),
+        )
+    else:
+        raise HTTPException(status_code=404, detail="Incorrect mode")
+
+    returns = Compute(db_number=db_num, results=result)
+    return returns
+
+
+@router.post("/compute_all/", response_model=Compute_all)
+def read_heroes(
+    numbers: List[int],
+    mode: Mode = "SUM",
+    db: DB = Depends(get_db),
+    authorized: bool = Depends(auth.validate),
+) -> Compute_all:
+    db_num = db.get_number()
+
+    results = []
+    for num in numbers:
+        if mode == "SUM":
+            results.append(Results(addition=(num + db_num)))
+        elif mode == "SUB":
+            results.append(Results(subtraction=(num - db_num)))
+        elif mode == "PROD":
+            results.append(Results(multiplication=(num * db_num)))
+        elif mode == "ALL":
+            results.append(
+                Results(
+                    addition=(num + db_num),
+                    subtraction=(num - db_num),
+                    multiplication=(num * db_num),
+                )
+            )
+        else:
+            raise HTTPException(status_code=404, detail="Incorrect mode")
+
+    returns = Compute_all(db_number=db_num, results=results)
+    return returns
